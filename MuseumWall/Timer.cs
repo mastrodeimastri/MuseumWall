f0b222516d3e9c13359a5b11e68b5bf08cc69439
mastrodeimastri <alvise.silve@gmail.com>
2022-12-02 17:32:38 +0100

    Redesing the filt system

---
+++ MuseumWall/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+
+namespace MuseumWall
+{
+    public partial class Common
+    {
+        protected int nScreens = 1;
+
+        // Questa funzione crea un timer di 10 secondi che
+        // verrà sfruttato per porre un tempo limite alle
+        // connessioni degli slave
+        protected void Timer()
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
+
